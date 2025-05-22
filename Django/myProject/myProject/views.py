69c57ac77778e36142afd0a91b9829cf76cd29f4
Khalid Mahmud <94211717+skhalidmahmud@users.noreply.github.com>
2025-05-22 13:05:24 +0600

    Add contact and portfolio pages

--- Django/myProject/myProject/views.py
+++ Django/myProject/myProject/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def homePage(req):
     return render(req, 'homePage.html')
+
+def contact(req):
+    return render(req, 'contact.html')
+
+def portfolio(req):
+    return render(req, 'portfolio.html')
