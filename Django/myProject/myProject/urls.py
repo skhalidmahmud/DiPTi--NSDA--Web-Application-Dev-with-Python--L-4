1a78eab8df9e609bd6d66e3e495d438fff7f7513
Khalid Mahmud <94211717+skhalidmahmud@users.noreply.github.com>
2025-05-22 12:46:01 +0600

    Add Template and link as homePage

--- Django/myProject/myProject/urls.py
+++ Django/myProject/myProject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myProject.views import homePage
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homePage, name='home'),
 ]
